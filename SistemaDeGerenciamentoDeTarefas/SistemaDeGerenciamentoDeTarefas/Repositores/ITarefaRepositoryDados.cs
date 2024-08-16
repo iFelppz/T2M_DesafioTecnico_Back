a35a0fea7377964cc0f2eb3c661adfd2a53e6b71
Felipe Cauê <felipecaue7@hotmail.com>
2024-08-16 03:13:14 -0300

    refactor: excluindo repositorio inutil

--- SistemaDeGerenciamentoDeTarefas/SistemaDeGerenciamentoDeTarefas/Repositores/ITarefaRepositoryDados.cs
+++
@@ -1,9 +0,0 @@
-﻿using SistemaDeGerenciamentoDeTarefas.Models;
-
-namespace SistemaDeGerenciamentoDeTarefas.Repositores
-{
-    public interface ITarefaRepositoryDados
-    {
-        void Adicionar(TarefaModel tarefaModel);
-    }
-}
